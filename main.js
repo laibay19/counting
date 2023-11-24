97c7c63e6e7b312c61a9c44f04bd2e2e9bd3d161
laibay <150113457+laibay19@users.noreply.github.com>
2023-11-24 13:38:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function counting(){
+    let num1 = +document.getElementById("from").value
+    let num2 = +document.getElementById("to").value
+
+    for ( i=from ; i<=to ; i++){
+
+        let output = document.getElementById("result").value
+            // console.log(i)
+        output.innerHTML = (output) 
+    }
+}
